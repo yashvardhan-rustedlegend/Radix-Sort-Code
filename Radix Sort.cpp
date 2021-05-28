0c5d83f8e0715983ad2a703e42d788d2a1f49c12
Yash Vardhan Tyagi <77291864+yashvardhan-rustedlegend@users.noreply.github.com>
2021-05-28 09:04:07 +0530

    Code

---
+++ Radix Sort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+	void radixsort(int a[],int n,int l)
+	{
+		int k,z,temp;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				k=a[i]/pow(10,l);
+				z=a[j]/pow(10,l);
+			
+				if(k%10>z%10)
+				{
+					temp=a[j];
+					a[j]=a[i];
+					a[i]=temp;
+				}
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			cout<<a[i]<<" ";
+		}
+		cout<<"\n";
+	}
+int main()
+{
+	int a[]={170,45,75,90,802,24,2,66};
+	int n=sizeof(a)/sizeof(a[0]);
+	int l=0,max=a[0];
+	int count=0;
+	for(int i=1;i<n;i++)
+	{
+		if(max<a[i])
+		{
+			max=a[i];
+		}
+	}
+	while(max!=0)
+	{
+		max=max/10;
+		count++;
+	}
+	for(int i=1;i<=count;i++)
+	{
+		radixsort(a,n,l);
+		l++;
+	}
+}
